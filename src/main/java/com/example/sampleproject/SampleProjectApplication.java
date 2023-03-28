17d6ffea7eaab5105788096ddcef1b8a0389e31d
Maximilian Matscher <matscher@cqse.eu>
2023-03-29 00:58:57 +0200

    Added spring security + content security policy for cross site scripting CORS

--- src/main/java/com/example/sampleproject/SampleProjectApplication.java
+++ src/main/java/com/example/sampleproject/SampleProjectApplication.java
@@ -5,24 +5,23 @@ import com.example.sub.B;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SampleProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SampleProjectApplication.class, args);
+    }
 
-	@GetMapping("/a")
-	public String returnA() {
-		return A.getValue();
-	}
+    @GetMapping("/a")
+    public String returnA() {
+        return A.getValue();
+    }
 
-	@GetMapping("/b")
-	public String returnB() {
-		return B.getValue();
-	}
+    @GetMapping("/b")
+    public String returnB() {
+        return B.getValue();
+    }
 }
---
+++ src/main/java/com/example/sampleproject/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.sampleproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy("default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';");
+        return http.build();
+    }
+}
