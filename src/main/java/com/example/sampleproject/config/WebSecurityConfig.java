77ee223bd171d65418a69603309bdc83b495aa06
Maximilian Matscher <matscher@cqse.eu>
2023-03-29 17:17:05 +0200

    Change security policy

--- src/main/java/com/example/sampleproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/sampleproject/config/WebSecurityConfig.java
@@ -12,7 +12,7 @@ public class WebSecurityConfig {
         http.headers()
                 .xssProtection()
                 .and()
-                .contentSecurityPolicy("default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval' 'unsafe-dynamic'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';");
+                .contentSecurityPolicy("default-src * data: blob: filesystem: about: ws: wss: 'unsafe-inline' 'unsafe-eval'; script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; connect-src * data: blob: 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src * data: blob: ; style-src * data: blob: 'unsafe-inline'; font-src * data: blob: 'unsafe-inline';");
 
         http.csrf().disable().authorizeRequests().anyRequest().permitAll();
         return http.build();
